b087869a9cc5fb9632a0f9320c848ad3e23ed6a6
Austin <a_leahy@acs.org>
2013-09-18 12:25:26 -0400

    removed references to old spelling class new code is in dictionary.cs

--- SpellChecker/SpellChecker.aspx.cs
+++ SpellChecker/SpellChecker.aspx.cs
@@ -12,21 +12,18 @@ namespace SpellChecker
         {
             Response.Clear();
             Response.ContentType = "application/json; charset=utf-8";
-
-            using (Spellchecker spellchecker = new Spellchecker("en_us.aff", "en_us.dic"))
+            using (Dictionary spellchecker = new Dictionary("en_us.aff", "en_us.dic"))
             {
                 string spellingAction = Request.Form["action"];
                 try
                 {
                     string json = null;
-
                     if (!string.IsNullOrEmpty(Request.Form["action"]))
                     {
-
                         if (spellchecker.GetType().GetMethod(Request.Form["action"]) != null)
                         {
-                            json = (typeof(Spellchecker).GetMethod(spellingAction)
-                                .Invoke(spellchecker, parameters: new object[] {Request})).ToString();
+                            json = (typeof(Dictionary).GetMethod(spellingAction)
+                                .Invoke(spellchecker, new object[] {Request})).ToString();
                         }
                     }
                     Response.Write(json);
@@ -39,43 +36,4 @@ namespace SpellChecker
             }
         }
     }
-
-    public class Spellchecker : Hunspell
-        {
-            public Spellchecker(string aff, string dic)
-                : base(aff, dic)
-            {
-                //other initialization stuff
-            }
-
-            public string get_incorrect_words(HttpRequest args)
-            {
-
-                var text = args.Form["text[]"];
-                char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
-                string[] words = text.Split(delimiterChars);
-                string outcome = null;
-                List<string> incorrectWords = new List<string>();
-                foreach (string word in words)
-                {
-                    var correct = Spell(word);
-                    if (!correct)
-                    {
-                        incorrectWords.Add(word);
-                    }
-                }
-                if (incorrectWords.Count > 0)
-                {
-                    outcome = "success";
-                }
-                var returndata = JsonConvert.SerializeObject(incorrectWords);
-                return "{\"outcome\":\"" + outcome + "\",\"data\":[" + returndata + "]}";
-            }
-            public string get_suggestions(HttpRequest args)
-            {
-                var word = args.Form["word"];
-                List<string> suggestions = Suggest(word);
-                return JsonConvert.SerializeObject(suggestions);
-            }
-        }
-    }
+}
