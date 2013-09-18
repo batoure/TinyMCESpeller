4842e5e8c46d5041f24821bcf041283e5004f217
Austin <a_leahy@acs.org>
2013-09-18 12:21:37 -0400

    Add seperate class for spelling opperations

---
+++ SpellChecker/Dictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using NHunspell;
+
+namespace SpellChecker
+{
+    public class Dictionary : Hunspell
+        {
+        public Dictionary(string aff, string dic)
+                : base(aff, dic)
+            {
+                //other initialization stuff
+            }
+        public string get_incorrect_words(string textBlock)
+        {
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+            string[] words = textBlock.Split(delimiterChars);
+            string outcome = null;
+            var incorrectWords = (from word in words let correct = Spell(word) where !correct select word).ToList();
+            if (incorrectWords.Count > 0)
+            {
+                outcome = "success";
+            }
+            var returndata = JsonConvert.SerializeObject(incorrectWords);
+            return "{\"outcome\":\"" + outcome + "\",\"data\":[" + returndata + "]}";
+        }
+        public string get_suggestions(string word)
+        {
+            List<string> suggestions = Suggest(word);
+            return JsonConvert.SerializeObject(suggestions);
+        }
+
+    }
+}
